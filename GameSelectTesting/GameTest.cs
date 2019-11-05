ea4ca4fb3d4e52e7951e42db4a29f247bbdc5aa5
Jake Hamilton <jhamilton@spartaglobal.com>
2019-11-05 17:58:24 +0000

    Homework based on test cases

---
+++ GameSelectTesting/GameTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace GameSelect.Test
+{
+    [TestFixture]
+    public class TestingGame
+    {
+        EnemyClass en = new EnemyClass();
+
+        [Test]
+        public void TestEnemyMethodEqual()
+        {
+            Assert.AreEqual(2, 2);
+        }
+        public void TestEnemyMethodNotEqual()
+        {
+            Assert.AreNotEqual(2, 3);
+        }
+    }
+}
---
+++ GameSelectTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GameSelectTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("GameSelectTesting")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a3005ad8-8fba-4a7c-80c7-325c4cebc2e7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
